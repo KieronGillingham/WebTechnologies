1bab4f7cc36d6de5a39daa95d2fd971554db6b0a
Kieron Gillingham <kierongillingham@gmail.com>
2019-03-21 11:17:17 +0000

    Implement subtotal for SolentStore

--- SolentStore/script/main.js
+++ SolentStore/script/main.js
@@ -1,18 +1,38 @@
+var form;
+var subTotal;
+var shipping = 0;
+var taxes = 0;
+var total = 0;
 
+window.onload = main;
 
-
-
-function main() {
-
-    //set up reference to our form, since 
-    var form = document.getElementById("productForm"); 
- 
-    
+function main()
+{
+    form = document.getElementById("productForm");
+    form.onchange = formChanged;
+    console.log("Main function complete.");
 }
 
-/**
-  onChange callback function
-**/
-function formChanged(event) {
-    var form = document.getElementById("productForm");     
+function formChanged(event)
+{
+    console.log(event);
+    
+
+    var products = document.getElementsByName("product");
+
+    subTotal = 0;
+    products.forEach(calcSubtotal);
+
+    document.getElementById("sub-total").innerHTML = "£" + subTotal + ".00";
+}
+
+function calcSubtotal(product)
+{
+  var inputs = product.getElementsByTagName("input");
+  var checkBox = inputs[0]; // Only works because add to cart is the first input tag under each product
+  var qty = inputs[1];
+  if (checkBox.checked)
+  {
+    subTotal += parseInt(product.dataset.price) * parseInt(qty.value);
+  }
 }
